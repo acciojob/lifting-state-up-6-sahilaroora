2e70ec1e8b0ef4aa1386986bb2580e000ceb54db
sahilaroora <sarora203@gmail.com>
2026-06-09 14:03:12 +0530

    tt

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,11 @@
 import React, { useState } from "react";
 
-export default function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "Todo 1", completed: false },
-    { id: 2, text: "Todo 2", completed: false }
-  ]);
-
-  const handleComplete = (id) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id
-          ? { ...todo, completed: true }
-          : todo
-      )
-    );
-  };
-
+function TodoList({ todos, handleComplete }) {
   return (
     <ul>
       {todos.map((todo) => (
         <li key={todo.id}>
-          {todo.text}
+          {todo.id} - {todo.completed ? "Completed" : "Pending"}
 
           {!todo.completed && (
             <button onClick={() => handleComplete(todo.id)}>
@@ -30,5 +15,32 @@ export default function App() {
         </li>
       ))}
     </ul>
+  );
+}
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, completed: false },
+    { id: 2, completed: false },
+    { id: 3, completed: false }
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: true }
+          : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <TodoList
+        todos={todos}
+        handleComplete={handleComplete}
+      />
+    </div>
   );
 }
